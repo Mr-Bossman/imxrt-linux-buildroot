44b350dd857cbfd1d772a75aba3466f7275e0be2
Jesse <mr.bossman075@gmail.com>
2021-05-01 21:32:36 -0400

    fixed boot cmds

--- board/IMXRT/v1/boot.cmd
+++ board/IMXRT/v1/boot.cmd
@@ -1,5 +1,5 @@
 setenv bootargs console=ttyS0,115200 root=/dev/ram0 earlycon debug ignore_loglevel
-load mmc 0:1 0x80600000 imxrt1050-evk.dtb
+load mmc 0:1 0x80800000 imxrt1050-evk.dtb
 load mmc 0:1 0x80000000 uImage
-load mmc 0:1 0x80700000 initramfs
-bootm 0x80000000 - 0x80600000
+load mmc 0:1 0x81000000 initramfs
+bootm 0x80000000 - 0x80800000
