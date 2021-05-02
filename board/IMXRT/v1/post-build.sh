040cdb50780af6eaef8aaa58d60c3f3cdbffc10b
Jesse <mr.bossman075@gmail.com>
2021-05-01 21:30:22 -0400

    boots linux

--- board/IMXRT/v1/post-build.sh
+++ board/IMXRT/v1/post-build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+rm output/images/uImage
+rm output/images/initramfs
 mkimage -A arm -O linux -T kernel -C none -a 0x80008000 -e 0x80008000 -n "Linux kernel" -d output/images/Image output/images/uImage
-mkimage -A arm -O linux -T ramdisk -C none -n "Root Filesystem" -d output/images/rootfs.cpio.uboot output/images/initramfs
+mkimage -A arm -O linux -T ramdisk -C none -n "Root Filesystem" -d output/images/rootfs.cpio output/images/initramfs
 ./support/scripts/genimage.sh $2 $3
