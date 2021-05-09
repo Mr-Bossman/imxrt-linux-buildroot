e77fe4b34a785e2eefc871975b2d4bb9a886235d
Jesse <mr.bossman075@gmail.com>
2021-05-08 22:09:57 -0400

    crashes at init

--- board/IMXRT/v1/boot.cmd
+++
@@ -1,5 +0,0 @@
-setenv bootargs console=ttyS0,115200 root=/dev/ram0 earlycon debug ignore_loglevel
-load mmc 0:1 0x80800000 imxrt1050-evk.dtb
-load mmc 0:1 0x80000000 uImage
-load mmc 0:1 0x81000000 initramfs
-bootm 0x80000000 0x81000000 0x80800000
--- board/IMXRT/v1/post-build.sh
+++ board/IMXRT/v1/post-build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 rm output/images/uImage
-rm output/images/initramfs
 mkimage -A arm -O linux -T kernel -C none -a 0x80008000 -e 0x80008000 -n "Linux kernel" -d output/images/Image output/images/uImage
-mkimage -A arm -O linux -T ramdisk -C none -n "Root Filesystem" -d output/images/rootfs.cpio output/images/initramfs
 ./support/scripts/genimage.sh $2 $3
