5c48d496790c5328535eb4ed0e32caa14eb9e025
Jesse Taube <Mr.Bossman075@gmail.com>
2021-12-04 20:45:56 -0500

    linux and boots

--- board/IMXRT/v1/post-build.sh
+++ board/IMXRT/v1/post-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm output/images/uImage
 cp ../board/IMXRT/v1/FLASHCONF output/images/
-cp output/build/linux-ada24fef7868b1f6b75249f589ad1a2e7af1d641/arch/arm/boot/dts/imxrt1050-evk.dtb output/images # bug in curent buildroot for kernel 5.16.x
-mkimage -A arm -O linux -T kernel -C none -a 0x80008000 -e 0x80008000 -n "Linux kernel" -d output/images/Image output/images/uImage
+cp output/build/linux-e6c04ee28e17171673a83a5aa12512dbfdc2e775/arch/arm/boot/dts/imxrt1050-evk.dtb output/images # bug in curent buildroot for kernel 5.16.x
+mkimage -A arm -O linux -T kernel -C none -a 0x6030D840 -e 0x6030D840 -n "Linux kernel" -d output/images/xipImage output/images/uImage
 ./support/scripts/genimage.sh $2 $3
