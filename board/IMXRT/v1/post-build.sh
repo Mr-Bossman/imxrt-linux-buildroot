7e68319c3c83c8d2b67b2c234c25e32b359a5007
Jesse Taube <Mr.Bossman075@gmail.com>
2021-12-04 19:32:38 -0500

    u-boot

--- board/IMXRT/v1/post-build.sh
+++ board/IMXRT/v1/post-build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 rm output/images/uImage
+cp ../board/IMXRT/v1/FLASHCONF output/images/
 cp output/build/linux-ada24fef7868b1f6b75249f589ad1a2e7af1d641/arch/arm/boot/dts/imxrt1050-evk.dtb output/images # bug in curent buildroot for kernel 5.16.x
 mkimage -A arm -O linux -T kernel -C none -a 0x80008000 -e 0x80008000 -n "Linux kernel" -d output/images/Image output/images/uImage
 ./support/scripts/genimage.sh $2 $3
