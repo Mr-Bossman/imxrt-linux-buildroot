a374e459002073df679f76e79d0108a47eabf63f
Jesse Taube <Mr.Bossman075@gmail.com>
2021-12-05 20:37:46 -0500

    boots from map

--- board/IMXRT/v1/post-build.sh
+++ board/IMXRT/v1/post-build.sh
@@ -2,5 +2,5 @@
 rm output/images/uImage
 cp ../board/IMXRT/v1/FLASHCONF output/images/
 cp output/build/linux-e6c04ee28e17171673a83a5aa12512dbfdc2e775/arch/arm/boot/dts/imxrt1050-evk.dtb output/images # bug in curent buildroot for kernel 5.16.x
-mkimage -A arm -O linux -T kernel -C none -a 0x6030D840 -e 0x6030D840 -n "Linux kernel" -d output/images/xipImage output/images/uImage
+mkimage -A arm -O linux -T kernel -C none -a 0x6008D840 -e 0x6008D840 -n "Linux kernel" -d output/images/xipImage output/images/uImage
 ./support/scripts/genimage.sh $2 $3
