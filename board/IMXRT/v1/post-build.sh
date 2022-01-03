72fd77dc325b0bdc0c28504d533b2990008ef369
Jesse Taube <Mr.Bossman075@gmail.com>
2022-01-03 18:36:01 -0500

    Update kernel & fixformating

--- board/IMXRT/v1/post-build.sh
+++ board/IMXRT/v1/post-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm output/images/uImage
-cp output/build/linux-ada24fef7868b1f6b75249f589ad1a2e7af1d641/arch/arm/boot/dts/imxrt1050-evk.dtb output/images # bug in curent buildroot for kernel 5.16.x
+cp output/build/linux-0ad2671c3c313582c6aeb5d0b06b89ca1b25a52d/arch/arm/boot/dts/imxrt1050-evk.dtb output/images # bug in curent buildroot for kernel 5.16.x
 mkimage -A arm -O linux -T kernel -C none -a 0x80008000 -e 0x80008000 -n "Linux kernel" -d output/images/Image output/images/uImage
 ./support/scripts/genimage.sh $2 $3
