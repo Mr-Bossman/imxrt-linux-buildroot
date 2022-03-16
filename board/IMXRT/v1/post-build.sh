a2217b41a3b71797958ea56971cda3e0d5d047de
Jesse Taube <Mr.Bossman075@gmail.com>
2022-03-16 12:12:22 -0400

    imxrt1170

--- board/IMXRT/v1/post-build.sh
+++ board/IMXRT/v1/post-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm output/images/uImage
-cp output/build/linux-b0cdf1678ef72b1846ca381e7f93a569abbe52a3/arch/arm/boot/dts/imxrt1050-evk.dtb output/images # bug in curent buildroot for kernel 5.16.x
+cp output/build/linux-3667c7e6fd8df804278256a08b417565fb494fb6/arch/arm/boot/dts/imxrt1170-evk.dtb output/images # bug in curent buildroot for kernel 5.16.x
 mkimage -A arm -O linux -T kernel -C none -a 0x80008000 -e 0x80008000 -n "Linux kernel" -d output/images/Image output/images/uImage
 ./support/scripts/genimage.sh $2 $3
