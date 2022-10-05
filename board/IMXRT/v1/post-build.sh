5c1b544d42d279519ba1d3c0b8aa509202a92187
Jesse Taube <Mr.Bossman075@gmail.com>
2022-10-04 22:56:25 -0400

    update

--- board/IMXRT/v1/post-build.sh
+++ board/IMXRT/v1/post-build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-rm output/images/uImage
-cp ../board/IMXRT/v1/FLASHCONF output/images/
-cp output/build/linux-b0cdf1678ef72b1846ca381e7f93a569abbe52a3/arch/arm/boot/dts/imxrt1050-evk.dtb output/images # bug in curent buildroot for kernel 5.16.x
-mkimage -A arm -O linux -T kernel -C none -a 0x6008D840 -e 0x6008D840 -n "Linux kernel" -d output/images/xipImage output/images/uImage
-./support/scripts/genimage.sh $2 $3
+MKIMAGE=$HOST_DIR/bin/mkimage
+cp ../board/IMXRT/v1/FLASHCONF $BINARIES_DIR
+$MKIMAGE -A arm -O linux -T kernel -C none -a 0x6008D840 -e 0x6008D840 -n "Linux kernel" -d $BINARIES_DIR/xipImage $BINARIES_DIR/uImage
