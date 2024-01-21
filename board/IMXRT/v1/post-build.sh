28c19f54a7278df09652e96288f1ff5d00ede280
Jesse Taube <Mr.Bossman075@gmail.com>
2024-01-21 15:17:54 -0500

    update

--- board/IMXRT/v1/post-build.sh
+++ board/IMXRT/v1/post-build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 MKIMAGE=$HOST_DIR/bin/mkimage
-cp ../board/IMXRT/v1/FLASHCONF $BINARIES_DIR
 $MKIMAGE -A arm -O linux -T kernel -C none -a 0x80008000 -e 0x80008000 -n "Linux kernel" -d $BINARIES_DIR/Image $BINARIES_DIR/uImage
